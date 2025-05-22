5b2a2d534e6b2f1ef8055b94cabcad1b1939b9cc
MunchiP <paulaviveusa@gmail.com>
2025-05-22 07:52:13 -0500

    se agrega backend del proyecto

---
+++ EcoChallengeBack/src/main/java/com/EcoChallenge/Servicios/UserService.java
@@ -0,0 +1,79 @@
+package com.EcoChallenge.Servicios;
+
+import com.EcoChallenge.Repositorio.UserRepository;
+import com.fasterxml.jackson.databind.DatabindContext;
+import com.EcoChallenge.Modelo.User;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+/*
+ *---@Service---
+ *utilizan normalmente para implementar la lógica de negocio de tu aplicación,
+ *separándola de la lógica de acceso a datos (@Repository) y del manejo de
+ *peticiones HTTP (@Controller).*/
+@Service
+public class UserService {
+    @Autowired
+    UserRepository userRepository;
+
+    /*
+     * optener user por email
+     * */
+
+    public User getUserEmail(String email){
+        return userRepository.findByEmail(email);
+    }
+
+
+    /*
+     * iniciar seccion
+     * */
+
+    public User inicarSesion(String email, String password) {
+        User user = getUserEmail(email);
+        if (user!= null && user.getPassword().equals(password)){
+            return user;
+        };
+
+        throw new IllegalArgumentException("Correo o contraseña incorrectos");
+    }
+
+    /*
+     * optener nombre y apellido
+     * */
+
+    public User getUserNombreApellido(){
+        DatabindContext session = null;
+        return (User) session.getAttribute("usuario");
+    }
+
+    /*
+     * guardar usuario
+     * */
+
+    public User postUser(User user){
+        return userRepository.save(user);
+    }
+
+    /*
+     * Actulizar un ususario
+     * */
+
+    public User putUser(User user){
+        return userRepository.save(user);
+    }
+
+
+    /*
+     * eliminar usuario
+     * */
+
+    public void deleteUser(long id){
+        userRepository.deleteById(id);
+    }
+
+}
